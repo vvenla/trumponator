479c449d616354e1cf903b9177720924cb73699f
Vili <vili.hatonen@helsinki.fi>
2018-10-29 11:43:39 +0200

    wrap the main to a function

--- trumponator.py
+++ trumponator.py
@@ -16,21 +16,26 @@ import sys
 MODEL_DIR = "models/"
 LABELS_AS_TEXT = {"BIGPOS": "BIG POSITIVE CHANGE ", "MEDPOS": "MEDIUM POSITIVE CHANGE ", "SMAPOS": "SMALL POSITIVE CHANGE ", "SMANEG": "SMALL NEGATIVE CHANGE ", "MEDNEG": "MEDIUM NEGATIVE CHANGE ",  "BIGNEG": "BIG NEGATIVE CHANGE "}
 
-model = fasttext.load_model(MODEL_DIR + 'all_model.bin')
 
-#print (model.labels)
+def main():
+    model = fasttext.load_model(MODEL_DIR + 'all_model.bin')
 
-if len(sys.argv) > 2:
-    print ("There are too many command line arguments. Did you forget to wrap the tweet with \" \" ?")
-elif len(sys.argv) > 1:
-    tweet = sys.argv[1]
-    print("Predict the stock reacton for tweet:\n\t", tweet)
-    print()
-    pred = model.predict(tweet)
-    pred = pred[0][0] #take the label out of lists
-    pred = pred[9:] #remove the "__label__" from front of it
-    pred_as_text = LABELS_AS_TEXT[pred]
-    print (pred_as_text + "predicted to next business day!")
-else:
-    print ("You didn't give a tweet to predict from!")
+    #print (model.labels)
+
+    if len(sys.argv) > 2:
+        print ("There are too many command line arguments. Did you forget to wrap the tweet with \" \" ?")
+    elif len(sys.argv) > 1:
+        tweet = sys.argv[1]
+        print("Predict the stock reacton for tweet:\n\t", tweet)
+        print()
+        pred = model.predict(tweet)
+        pred = pred[0][0] #take the label out of lists
+        pred = pred[9:] #remove the "__label__" from front of it
+        pred_as_text = LABELS_AS_TEXT[pred]
+        print (pred_as_text + "predicted to next business day!")
+    else:
+        print ("You didn't give a tweet to predict from!")
+
+if __name__ == "__main__":
+    main()
 
