097a3c3e239c30c311f6cb0f03d22b8bd18820bb
Vili <vili.hatonen@helsinki.fi>
2018-10-29 11:17:24 +0200

    trumponator predicts from a tweet - no cleaning!

--- trumponator.py
+++ trumponator.py
@@ -11,10 +11,22 @@
 """
 
 import fasttext
+import sys
 
 MODEL_DIR = "models/"
 
 model = fasttext.load_model(MODEL_DIR + 'all_model.bin')
 
-print (model.labels)
+#print (model.labels)
+
+if len(sys.argv) > 1:
+    tweet = sys.argv[1]
+    print("Predict the stock reacton for tweet:\n\t", tweet)
+    print()
+    pred = model.predict(tweet)
+    pred = pred[0][0] #take the label out of lists
+    pred = pred[9:] #remove the "__label__" from front of it
+    print ("The stock will change " + pred + " on the next business day!")
+else:
+    print ("You didn't give a tweet to predict from!")
 
